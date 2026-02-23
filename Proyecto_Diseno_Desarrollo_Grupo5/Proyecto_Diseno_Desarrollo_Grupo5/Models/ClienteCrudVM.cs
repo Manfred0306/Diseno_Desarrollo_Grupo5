d43c1ae2a081e55a9e44a61c734e4d4a9107ee77
ChrisMurillo06 <christophermurillo155@gmail.com>
2026-02-23 14:29:18 -0600

    Sprint3 Clientes

---
+++ Proyecto_Diseno_Desarrollo_Grupo5/Proyecto_Diseno_Desarrollo_Grupo5/Models/ClienteCrudVM.cs
@@ -0,0 +1,49 @@
+﻿using Proyecto_Diseno_Desarrollo_Grupo5.EF;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Proyecto_Diseno_Desarrollo_Grupo5.Models
+{
+    public class ClienteFilaVM
+    {
+        public int ID_CLIENTE { get; set; }
+        public string NOMBRE { get; set; }
+        public string TELEFONO { get; set; }
+        public string CORREO { get; set; }
+        public string DIRECCION { get; set; }
+        public int ID_ESTADO { get; set; }
+        public string ESTADO { get; set; }
+    }
+
+    public class ClienteCrudVM
+    {
+        // Lista
+        public List<ClienteFilaVM> Clientes { get; set; } = new List<ClienteFilaVM>();
+
+        // Para combos
+        public List<SelectListItem> Estados { get; set; } = new List<SelectListItem>();
+
+        // Form (Create/Edit)
+        public int ID_CLIENTE { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NOMBRE { get; set; }
+
+        [StringLength(30)]
+        public string TELEFONO { get; set; }
+
+        [StringLength(120)]
+        [EmailAddress]
+        public string CORREO { get; set; }
+
+        [StringLength(200)]
+        public string DIRECCION { get; set; }
+
+        public int ID_ESTADO { get; set; } = 1;
+
+        // Búsqueda
+        public string Q { get; set; }
+    }
+}
